12f6fc30f16ac860caf0008f26914c672f234844
artnomic <arthurpe44@gmail.com>
2024-12-09 14:08:02 -0300

    config Containers

---
+++ src/containers/ListaDeTarefas/index.tsx
@@ -0,0 +1,9 @@
+const ListaDeTarefas = () => (
+  <main>
+    <p>
+      2 tarefas marcadas como: &ldquo;Categoria&rdquo; e &ldquo;Termo&rdquo;
+    </p>
+  </main>
+)
+
+export default ListaDeTarefas
